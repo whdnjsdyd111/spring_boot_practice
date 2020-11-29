f58c6cd80e86b5152d12c4f06e8bfac5eb8ab62b
whdnjsdyd111 <66655578+whdnjsdyd111@users.noreply.github.com>
2020-11-29 17:43:18 +0900

    Initial commit

---
+++ src/main/java/com/example/spring_boot_introduction_practice/SpringBootIntroductionPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring_boot_introduction_practice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootIntroductionPracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootIntroductionPracticeApplication.class, args);
+    }
+
+}
